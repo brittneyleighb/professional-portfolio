10bd11ea3fa1be8b006e9ef0cc4f647a71d0c316
brittneyleighb <117863391+brittneyleighb@users.noreply.github.com>
2022-12-18 21:57:12 -0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -106,7 +106,7 @@ const Footer = () => {
 
       <StyledCredit tabindex="-1">
         <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Iterated by Brittney Banning</div>
+          <div>Designed by Brittany Chiang &amp;, Iterated with ♥️ and ☕️ by Brittney Banning</div>
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
