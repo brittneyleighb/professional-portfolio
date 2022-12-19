fff60749e32b82daa347c51937407005232728d8
brittneyleighb <117863391+brittneyleighb@users.noreply.github.com>
2022-12-19 03:05:40 -0700

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -143,20 +143,20 @@ const About = () => {
               Fast-forward to today, and I’ve had the privilege of creating and owning a Shopify storefront,{' '}
               <a href="https://theslothboutique.com/">The Sloth Boutique</a>,{' '}
               <a href="https://www.etsy.com/shop/TheSlothBoutique">our Etsy shop</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              <a href="https://statementearringco.com/">a small E-Commerce project to learn SEO, product photography, and other digital marketing skills</a>, and{' '}
+              <a href="https://www.tiktok.com/@bellusvitatravel">other creative pursuits</a>. My
+              interests currently are in UX Design for both mobile and web, E-Commerce, HTML & CSS webpages, Android app development, video game development, digital design, 
+              graphic design, and learning programming in various language, with a recent focus on Lua, Python, and learning how to use Blender and Unreal Engine.
+              I am constanstly learning new skills and am always brimming with ideas.
             </p>
 
             <p>
               I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
+              <a href="https://github.com/brittneyleighb/Harvard-CS50">
+                took Harvard CS50
               </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              to learn the basics of computer programming
+              &amp; coding.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
