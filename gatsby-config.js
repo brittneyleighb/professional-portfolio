410625822ec0c938d5ba66c248215de31b061591
brittneyleighb <117863391+brittneyleighb@users.noreply.github.com>
2022-12-18 21:35:38 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Brittney Banning,
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Brittany Chiang is a UX who specializes in creating exceptional digital experiences.',
+    siteUrl: 'https://brittneyleighb.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@bellusvita_',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,11 +20,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Brittney Banning',
+        short_name: 'Brittney Banning',
         start_url: '/',
-        background_color: config.colors.darkNavy,
-        theme_color: config.colors.navy,
+        background_color: config.colors.black,
+        theme_color: config.colors.black,
         display: 'minimal-ui',
         icon: 'src/images/logo.png',
       },
