ee1d2a6c2c7e2905dfc19b70b992c154ce2ca072
brittneyleighb <117863391+brittneyleighb@users.noreply.github.com>
2022-12-19 19:17:20 -0700

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -170,7 +170,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="https://live.staticflickr.com/65535/52563218494_713546bba2_b.jpg"
+              src="https://live.staticflickr.com/65535/52573898702_573b65c645_z.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
