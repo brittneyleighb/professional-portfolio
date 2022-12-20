0f8a182abe449e38446e6f01f4ec98f285c1deb5
brittneyleighb <117863391+brittneyleighb@users.noreply.github.com>
2022-12-19 20:16:02 -0700

    Update contact.js

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,7 +60,7 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Currently open for part-time/full-time work, freelance work, collaborating on a project, or any new opportunities, my inbox is always open.
+        Currently open for job opportunities, freelance work, collaborating on a project, or any new opportunities. My inbox is always open.
         Whether you have a question or just want to say hi, I’ll try my best to get back to you!
       </p>
 
