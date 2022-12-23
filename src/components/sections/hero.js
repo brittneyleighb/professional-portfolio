6903132a30165d9b19856caf0b36ef99ec9438ac
brittneyleighb <117863391+brittneyleighb@users.noreply.github.com>
2022-12-22 21:54:02 -0700

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -61,7 +61,7 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Brittney Banning.</h2>;
-  const three = <h3 className="big-heading">I design things for the web.</h3>;
+  const three = <h3 className="big-heading">I design things for fun experiences.</h3>;
   const four = (
     <>
       <p>
